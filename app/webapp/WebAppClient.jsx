a82b9745b57bbb4e72dd2d33e84fd96c3a8f27f6
knowwssuz-byte <knowwssuz@gmail.com>
2026-07-05 10:41:58 +0500

    Update WebAppClient.jsx

--- app/webapp/WebAppClient.jsx
+++ app/webapp/WebAppClient.jsx
@@ -828,24 +828,10 @@ export default function WebAppClient() {
     return (
       <div className="minimal-loader-screen">
         <div className="minimal-loader-bg" />
-        <div className="loader" aria-label="Loading">
-          <div className="loader__balls">
-            <div className="loader__balls__group">
-              <div className="ball item1" />
-              <div className="ball item1" />
-              <div className="ball item1" />
-            </div>
-            <div className="loader__balls__group">
-              <div className="ball item2" />
-              <div className="ball item2" />
-              <div className="ball item2" />
-            </div>
-            <div className="loader__balls__group">
-              <div className="ball item3" />
-              <div className="ball item3" />
-              <div className="ball item3" />
-            </div>
-          </div>
+        <div className="loader box-loader" aria-label="Loading">
+          <div className="box1" />
+          <div className="box2" />
+          <div className="box3" />
         </div>
       </div>
     );
