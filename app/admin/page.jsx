1a888fc3752a9fe78fb0ddd015c6de83c9be49ea
knowwssuz-byte <knowwssuz@gmail.com>
2026-07-05 07:53:20 +0500

    Create page.jsx

---
+++ app/admin/page.jsx
@@ -0,0 +1,10 @@
+import AdminClient from './AdminClient';
+
+export const metadata = {
+  title: 'Gift Myst Admin',
+  description: 'Secure browser admin panel',
+};
+
+export default function AdminPage() {
+  return <AdminClient />;
+}
