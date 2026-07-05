c12a900138828bcaeecf727b03a8697423ec098c
knowwssuz-byte <knowwssuz@gmail.com>
2026-07-05 11:13:55 +0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="uz">
       <head>
-        <script src="https://telegram.org/js/telegram-web-app.js" async />
+        <script src="https://telegram.org/js/telegram-web-app.js" />
       </head>
       <body>{children}</body>
     </html>
